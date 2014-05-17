d0765b4fd267c88fc5d196e71a4e1d20c515bfd1
Zak Benjamin <zak@zaks-mbp.lan>
2014-05-17 22:12:52 +0100

    Changed the original laser sound to a new pacman sound

--- js/level.js
+++ js/level.js
@@ -57,7 +57,7 @@
   }
 
   $(function() {
-    GameAudio.load({ 'fire' : 'media/laser.ogg', 'die' : 'media/explosion.ogg' }, 
+    GameAudio.load({ 'fire' : 'media/waka.ogg', 'die' : 'media/explosion.ogg' }, 
                    function() { 
                        Game.initialize("#gameboard", levelData, spriteData,
                                       { "start": startGame,
