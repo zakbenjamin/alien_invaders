1f8bdcdc35a670367919f5b2a089ae491eebec4c
Zak Benjamin <zak@Zaks-MacBook-Pro.local>
2014-05-19 14:49:46 +0100

    Add left and right game panels and change title

--- js/level.js
+++ js/level.js
@@ -26,13 +26,13 @@
           [0,0,0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0,0,0,0,0],
-          [0,3,3,3,3,3,3,3,3,3,0],
-          [0,3,3,3,3,3,3,3,3,3,0],
+          [0,0,3,3,3,3,3,3,3,0,0],
+          [0,0,3,3,3,3,3,3,3,0,0],
           [0,2,2,2,2,2,2,2,2,2,0],
           [0,2,1,1,1,1,1,1,1,2,0],
-          [0,2,1,1,1,1,1,1,1,2,0],
           [1,1,1,1,1,1,1,1,1,1,1],
-          [1,1,1,1,1,1,1,1,1,1,1]]};																/* 1 & 2 represents Alien 1 & 2, whilst 0 represents no alien. Depending on each level, you can change from bottom top what type, how many and what direction the alines come from.  */
+          [1,1,1,1,1,1,1,1,1,1,1],
+          [0,0,0,0,0,0,0,0,0,0,0]]};																/* 1 & 2 represents Alien 1 & 2, whilst 0 represents no alien. Depending on each level, you can change from bottom top what type, how many and what direction the alines come from.  */
 
   var spriteData = {
     'alien1': { sx: 0,  sy: 0,  w: 20, h: 18, cls: Alien, frames: 2 }, 								
@@ -49,6 +49,7 @@
                                  });
     Game.loadBoard(screen);
     Game.loop();
+    GameAudio.play('start'); 
   }
 
   function endGame() {
@@ -69,7 +70,7 @@
   }
 
   $(function() {
-    GameAudio.load({ 'fire' : 'media/waka.ogg', 'die' : 'media/laserdeath.ogg' }, 
+    GameAudio.load({ 'fire' : 'media/waka.ogg', 'die' : 'media/laserdeath.ogg', 'start' : 'media/startsound.ogg'}, 
                    function() { 
                        Game.initialize("#gameboard", levelData, spriteData,
                                       { "start": startGame,
